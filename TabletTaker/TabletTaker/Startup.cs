dc993c777ef630dd825062017c7d697d6997cea4
Carl Dean <carlanthony2@hotmail.com>
2016-12-28 15:31:15 +0000

    Initial Commit

---
+++ TabletTaker/TabletTaker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TabletTaker.Startup))]
+namespace TabletTaker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
