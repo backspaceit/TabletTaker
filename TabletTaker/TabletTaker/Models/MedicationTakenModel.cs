460093bb3d1644a7910e8069c375be880ad9254b
Carl Dean <carlanthony2@hotmail.com>
2016-12-29 08:34:52 +0000

    Added MedicationTaken Model

---
+++ TabletTaker/TabletTaker/Models/MedicationTakenModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TabletTaker.Models
+{
+    public class MedicationTakenModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Dosage { get; set; }
+        public int NumberOfDosages { get; set; }
+
+    }
+}
